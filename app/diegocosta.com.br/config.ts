b4f0827131369070326d45f03723336b8228533e
Diego Costa <diego@diegocosta.me>
2026-08-19 11:48:02 -0300

    Reorder and update links in config.ts

--- app/diegocosta.com.br/config.ts
+++ app/diegocosta.com.br/config.ts
@@ -8,11 +8,11 @@ const profile = {
   avatar: 'https://avatars.githubusercontent.com/u/3134422?v=4',
   bio: 'Sou um engineering manager apaixonado por transformar tecnologia em impacto real. Tenho mais de 10 anos de experiência em engenharia de software, atuando em grandes empresas e startups em rápido crescimento.\nMeus principais interesses incluem liderança de engenharia, gestão de pessoas e a promoção da diversidade e inclusão no setor de tecnologia.\nSe você quiser saber mais sobre mim, confira os links abaixo!',
   links: [
-    { label: 'Linkedin', url: 'https://linkedin.com/in/diegocoxta' },
-    { label: 'Github', url: 'https://github.com/diegocoxta' },
     { label: 'Blog', url: '/blog' },
     { label: 'Currículo', url: '/cv' },
     { label: 'E-mail', url: 'mailto:diego@diegocosta.com.br' },
+    { label: 'Linkedin', url: 'https://linkedin.com/in/diegocoxta' },
+    { label: 'Github', url: 'https://github.com/diegocoxta' },
     { label: 'Mais', url: 'https://diegocoxta.com' },
   ],
   repository: npmPackage.repository,
